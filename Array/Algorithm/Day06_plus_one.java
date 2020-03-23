8913811960575d8099452cad83a4605147b1407b
mao <1269902942@qq.com>
2020-03-23 19:51:06 +0800

    66 整数由数组表示，显示该整数加一的数

---
+++ Array/Algorithm/Day06_plus_one.java
@@ -0,0 +1,15 @@
+package Algorithm;
+
+public class Day06_plus_one {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) return digits;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+}
